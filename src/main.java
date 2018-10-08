1b87723ca14ae69f31ff0175d1f0f68c5d309919
aldilesmana <aldilesmana181@gmail.com>
2018-10-09 00:37:19 +0700

    first commit

---
+++ src/main.java
@@ -0,0 +1,18 @@
+//NAMA  :Aldi Lesmana
+//NIM   :10117108  
+//KELAS :IF-3
+
+/**
+ *
+ * @author Aldi Lesmana
+ */
+public class main {
+    public static void main(String[] args) {
+        Lingkaran ling = new Lingkaran();
+
+        System.out.println("=======Perhitungan Lingkaran=======");
+        ling.validasiInput();
+        ling.hasilPerhitungan(ling.diameter);
+    }
+    
+}
